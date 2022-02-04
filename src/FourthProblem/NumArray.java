7a39cc3ad91ba994326d19a561001b0c120a06c5
Komarov Andrey <komarovandrey396@gmail.com>
2022-02-04 17:05:25 +0300

    added 4 problem

---
+++ src/FourthProblem/NumArray.java
@@ -0,0 +1,606 @@
+package FourthProblem;
+
+import java.util.Scanner;
+
+public class NumArray {
+    public static void createDigitOne() {
+        String[][] digit1;
+        digit1 = new String[8][5];
+
+        digit1[0][0] = " ";
+        digit1[0][1] = " ";
+        digit1[0][2] = "*";
+        digit1[0][3] = " ";
+        digit1[0][4] = " ";
+        digit1[1][0] = " ";
+        digit1[1][1] = " ";
+        digit1[1][2] = "*";
+        digit1[1][3] = " ";
+        digit1[1][4] = " ";
+        digit1[2][0] = " ";
+        digit1[2][1] = "*";
+        digit1[2][2] = "*";
+        digit1[2][3] = " ";
+        digit1[2][4] = " ";
+        digit1[3][0] = "*";
+        digit1[3][1] = " ";
+        digit1[3][2] = "*";
+        digit1[3][3] = " ";
+        digit1[3][4] = " ";
+        digit1[4][0] = " ";
+        digit1[4][1] = " ";
+        digit1[4][2] = "*";
+        digit1[4][3] = " ";
+        digit1[4][4] = " ";
+        digit1[5][0] = " ";
+        digit1[5][1] = " ";
+        digit1[5][2] = "*";
+        digit1[5][3] = " ";
+        digit1[5][4] = " ";
+        digit1[6][0] = " ";
+        digit1[6][1] = " ";
+        digit1[6][2] = "*";
+        digit1[6][3] = " ";
+        digit1[6][4] = " ";
+        digit1[7][0] = "*";
+        digit1[7][1] = "*";
+        digit1[7][2] = "*";
+        digit1[7][3] = "*";
+        digit1[7][4] = "*";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit1[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitTwo() {
+        String[][] digit2;
+        digit2 = new String[8][5];
+
+        digit2[0][0] = " ";
+        digit2[0][1] = "*";
+        digit2[0][2] = "*";
+        digit2[0][3] = "*";
+        digit2[0][4] = " ";
+        digit2[1][0] = "*";
+        digit2[1][1] = " ";
+        digit2[1][2] = " ";
+        digit2[1][3] = " ";
+        digit2[1][4] = "*";
+        digit2[2][0] = " ";
+        digit2[2][1] = " ";
+        digit2[2][2] = " ";
+        digit2[2][3] = " ";
+        digit2[2][4] = "*";
+        digit2[3][0] = " ";
+        digit2[3][1] = " ";
+        digit2[3][2] = " ";
+        digit2[3][3] = "*";
+        digit2[3][4] = " ";
+        digit2[4][0] = " ";
+        digit2[4][1] = " ";
+        digit2[4][2] = "*";
+        digit2[4][3] = " ";
+        digit2[4][4] = " ";
+        digit2[5][0] = " ";
+        digit2[5][1] = "*";
+        digit2[5][2] = " ";
+        digit2[5][3] = " ";
+        digit2[5][4] = " ";
+        digit2[6][0] = "*";
+        digit2[6][1] = " ";
+        digit2[6][2] = " ";
+        digit2[6][3] = " ";
+        digit2[6][4] = " ";
+        digit2[7][0] = "*";
+        digit2[7][1] = "*";
+        digit2[7][2] = "*";
+        digit2[7][3] = "*";
+        digit2[7][4] = "*";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit2[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitThree() {
+        String[][] digit3;
+        digit3 = new String[8][5];
+
+        digit3[0][0] = " ";
+        digit3[0][1] = "*";
+        digit3[0][2] = "*";
+        digit3[0][3] = "*";
+        digit3[0][4] = " ";
+        digit3[1][0] = "*";
+        digit3[1][1] = " ";
+        digit3[1][2] = " ";
+        digit3[1][3] = " ";
+        digit3[1][4] = "*";
+        digit3[2][0] = " ";
+        digit3[2][1] = " ";
+        digit3[2][2] = " ";
+        digit3[2][3] = " ";
+        digit3[2][4] = "*";
+        digit3[3][0] = " ";
+        digit3[3][1] = " ";
+        digit3[3][2] = "*";
+        digit3[3][3] = "*";
+        digit3[3][4] = " ";
+        digit3[4][0] = " ";
+        digit3[4][1] = " ";
+        digit3[4][2] = " ";
+        digit3[4][3] = " ";
+        digit3[4][4] = "*";
+        digit3[5][0] = " ";
+        digit3[5][1] = " ";
+        digit3[5][2] = " ";
+        digit3[5][3] = " ";
+        digit3[5][4] = "*";
+        digit3[6][0] = "*";
+        digit3[6][1] = " ";
+        digit3[6][2] = " ";
+        digit3[6][3] = " ";
+        digit3[6][4] = "*";
+        digit3[7][0] = " ";
+        digit3[7][1] = "*";
+        digit3[7][2] = "*";
+        digit3[7][3] = "*";
+        digit3[7][4] = " ";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit3[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitFour() {
+        String[][] digit4;
+        digit4 = new String[8][5];
+
+        digit4[0][0] = "*";
+        digit4[0][1] = "";
+        digit4[0][2] = "";
+        digit4[0][3] = "";
+        digit4[0][4] = "*";
+        digit4[1][0] = "*";
+        digit4[1][1] = "";
+        digit4[1][2] = "";
+        digit4[1][3] = "";
+        digit4[1][4] = "*";
+        digit4[2][0] = "*";
+        digit4[2][1] = "";
+        digit4[2][2] = "";
+        digit4[2][3] = "";
+        digit4[2][4] = "*";
+        digit4[3][0] = "*";
+        digit4[3][1] = "";
+        digit4[3][2] = "";
+        digit4[3][3] = "";
+        digit4[3][4] = "*";
+        digit4[4][0] = "*";
+        digit4[4][1] = "*";
+        digit4[4][2] = "*";
+        digit4[4][3] = "*";
+        digit4[4][4] = "*";
+        digit4[5][0] = "";
+        digit4[5][1] = "";
+        digit4[5][2] = "";
+        digit4[5][3] = "";
+        digit4[5][4] = "*";
+        digit4[6][0] = "";
+        digit4[6][1] = "";
+        digit4[6][2] = "";
+        digit4[6][3] = "";
+        digit4[6][4] = "*";
+        digit4[7][0] = "";
+        digit4[7][1] = "";
+        digit4[7][2] = "";
+        digit4[7][3] = "";
+        digit4[7][4] = "*";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit4[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void createDigitFive() {
+        String[][] digit5;
+        digit5 = new String[8][5];
+
+        digit5[0][0] = "*";
+        digit5[0][1] = "*";
+        digit5[0][2] = "*";
+        digit5[0][3] = "*";
+        digit5[0][4] = "*";
+        digit5[1][0] = "*";
+        digit5[1][1] = "";
+        digit5[1][2] = "";
+        digit5[1][3] = "";
+        digit5[1][4] = "";
+        digit5[2][0] = "*";
+        digit5[2][1] = "";
+        digit5[2][2] = "";
+        digit5[2][3] = "";
+        digit5[2][4] = "";
+        digit5[3][0] = "*";
+        digit5[3][1] = "*";
+        digit5[3][2] = "*";
+        digit5[3][3] = "*";
+        digit5[3][4] = "*";
+        digit5[4][0] = "";
+        digit5[4][1] = "";
+        digit5[4][2] = "";
+        digit5[4][3] = "";
+        digit5[4][4] = "*";
+        digit5[5][0] = "";
+        digit5[5][1] = "";
+        digit5[5][2] = "";
+        digit5[5][3] = "";
+        digit5[5][4] = "*";
+        digit5[6][0] = "*";
+        digit5[6][1] = "";
+        digit5[6][2] = "";
+        digit5[6][3] = "";
+        digit5[6][4] = "*";
+        digit5[7][0] = "";
+        digit5[7][1] = "*";
+        digit5[7][2] = "*";
+        digit5[7][3] = "*";
+        digit5[7][4] = "";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit5[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitSix() {
+        String[][] digit6;
+        digit6 = new String[8][5];
+
+        digit6[0][0] = "";
+        digit6[0][1] = "*";
+        digit6[0][2] = "*";
+        digit6[0][3] = "*";
+        digit6[0][4] = "";
+        digit6[1][0] = "*";
+        digit6[1][1] = "";
+        digit6[1][2] = "";
+        digit6[1][3] = "";
+        digit6[1][4] = "*";
+        digit6[2][0] = "*";
+        digit6[2][1] = "";
+        digit6[2][2] = "";
+        digit6[2][3] = "";
+        digit6[2][4] = "";
+        digit6[3][0] = "*";
+        digit6[3][1] = "*";
+        digit6[3][2] = "*";
+        digit6[3][3] = "*";
+        digit6[3][4] = "";
+        digit6[4][0] = "*";
+        digit6[4][1] = "";
+        digit6[4][2] = "";
+        digit6[4][3] = "";
+        digit6[4][4] = "*";
+        digit6[5][0] = "*";
+        digit6[5][1] = "";
+        digit6[5][2] = "";
+        digit6[5][3] = "";
+        digit6[5][4] = "*";
+        digit6[6][0] = "*";
+        digit6[6][1] = "";
+        digit6[6][2] = "";
+        digit6[6][3] = "";
+        digit6[6][4] = "*";
+        digit6[7][0] = "";
+        digit6[7][1] = "*";
+        digit6[7][2] = "*";
+        digit6[7][3] = "*";
+        digit6[7][4] = "";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit6[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitSeven() {
+        String[][] digit7;
+        digit7 = new String[8][5];
+
+        digit7[0][0] = "*";
+        digit7[0][1] = "*";
+        digit7[0][2] = "*";
+        digit7[0][3] = "*";
+        digit7[0][4] = "*";
+        digit7[1][0] = "";
+        digit7[1][1] = "";
+        digit7[1][2] = "";
+        digit7[1][3] = "";
+        digit7[1][4] = "*";
+        digit7[2][0] = "";
+        digit7[2][1] = "";
+        digit7[2][2] = "";
+        digit7[2][3] = "";
+        digit7[2][4] = "*";
+        digit7[3][0] = "";
+        digit7[3][1] = "";
+        digit7[3][2] = "";
+        digit7[3][3] = "*";
+        digit7[3][4] = "";
+        digit7[4][0] = "";
+        digit7[4][1] = "";
+        digit7[4][2] = "*";
+        digit7[4][3] = "";
+        digit7[4][4] = "";
+        digit7[5][0] = "";
+        digit7[5][1] = "*";
+        digit7[5][2] = "";
+        digit7[5][3] = "";
+        digit7[5][4] = "";
+        digit7[6][0] = "*";
+        digit7[6][1] = "";
+        digit7[6][2] = "";
+        digit7[6][3] = "";
+        digit7[6][4] = "";
+        digit7[7][0] = "*";
+        digit7[7][1] = "";
+        digit7[7][2] = "";
+        digit7[7][3] = "";
+        digit7[7][4] = "";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit7[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitEight() {
+        String[][] digit8;
+        digit8 = new String[8][5];
+
+        digit8[0][0] = "";
+        digit8[0][1] = "*";
+        digit8[0][2] = "*";
+        digit8[0][3] = "*";
+        digit8[0][4] = "";
+        digit8[1][0] = "*";
+        digit8[1][1] = "";
+        digit8[1][2] = "";
+        digit8[1][3] = "";
+        digit8[1][4] = "*";
+        digit8[2][0] = "*";
+        digit8[2][1] = "";
+        digit8[2][2] = "";
+        digit8[2][3] = "";
+        digit8[2][4] = "*";
+        digit8[3][0] = "";
+        digit8[3][1] = "*";
+        digit8[3][2] = "*";
+        digit8[3][3] = "*";
+        digit8[3][4] = "";
+        digit8[4][0] = "*";
+        digit8[4][1] = "";
+        digit8[4][2] = "";
+        digit8[4][3] = "";
+        digit8[4][4] = "*";
+        digit8[5][0] = "*";
+        digit8[5][1] = "";
+        digit8[5][2] = "";
+        digit8[5][3] = "";
+        digit8[5][4] = "*";
+        digit8[6][0] = "*";
+        digit8[6][1] = "";
+        digit8[6][2] = "";
+        digit8[6][3] = "";
+        digit8[6][4] = "*";
+        digit8[7][0] = "";
+        digit8[7][1] = "*";
+        digit8[7][2] = "*";
+        digit8[7][3] = "*";
+        digit8[7][4] = "";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit8[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitNine() {
+        String[][] digit9;
+        digit9 = new String[8][5];
+
+        digit9[0][0] = "";
+        digit9[0][1] = "*";
+        digit9[0][2] = "*";
+        digit9[0][3] = "*";
+        digit9[0][4] = "";
+        digit9[1][0] = "*";
+        digit9[1][1] = "";
+        digit9[1][2] = "";
+        digit9[1][3] = "";
+        digit9[1][4] = "*";
+        digit9[2][0] = "*";
+        digit9[2][1] = "";
+        digit9[2][2] = "";
+        digit9[2][3] = "";
+        digit9[2][4] = "*";
+        digit9[3][0] = "*";
+        digit9[3][1] = "";
+        digit9[3][2] = "";
+        digit9[3][3] = "";
+        digit9[3][4] = "*";
+        digit9[4][0] = "";
+        digit9[4][1] = "*";
+        digit9[4][2] = "*";
+        digit9[4][3] = "*";
+        digit9[4][4] = "*";
+        digit9[5][0] = "";
+        digit9[5][1] = "";
+        digit9[5][2] = "";
+        digit9[5][3] = "";
+        digit9[5][4] = "*";
+        digit9[6][0] = "*";
+        digit9[6][1] = "";
+        digit9[6][2] = "";
+        digit9[6][3] = "";
+        digit9[6][4] = "*";
+        digit9[7][0] = "";
+        digit9[7][1] = "*";
+        digit9[7][2] = "*";
+        digit9[7][3] = "*";
+        digit9[7][4] = "";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit9[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createDigitZero() {
+        String[][] digit0;
+        digit0 = new String[8][5];
+
+        digit0[0][0] = "";
+        digit0[0][1] = "*";
+        digit0[0][2] = "*";
+        digit0[0][3] = "*";
+        digit0[0][4] = "";
+        digit0[1][0] = "*";
+        digit0[1][1] = "";
+        digit0[1][2] = "";
+        digit0[1][3] = "";
+        digit0[1][4] = "*";
+        digit0[2][0] = "*";
+        digit0[2][1] = "";
+        digit0[2][2] = "";
+        digit0[2][3] = "";
+        digit0[2][4] = "*";
+        digit0[3][0] = "*";
+        digit0[3][1] = "";
+        digit0[3][2] = "";
+        digit0[3][3] = "";
+        digit0[3][4] = "*";
+        digit0[4][0] = "*";
+        digit0[4][1] = "";
+        digit0[4][2] = "";
+        digit0[4][3] = "";
+        digit0[4][4] = "*";
+        digit0[5][0] = "*";
+        digit0[5][1] = "";
+        digit0[5][2] = "";
+        digit0[5][3] = "";
+        digit0[5][4] = "*";
+        digit0[6][0] = "*";
+        digit0[6][1] = "";
+        digit0[6][2] = "";
+        digit0[6][3] = "";
+        digit0[6][4] = "*";
+        digit0[7][0] = "";
+        digit0[7][1] = "*";
+        digit0[7][2] = "*";
+        digit0[7][3] = "*";
+        digit0[7][4] = "";
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(digit0[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void numToStar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any positive digit. ");
+
+        String input = scanner.next();
+        char[] userCh = input.toCharArray();
+
+        for (int i = 0; i < userCh.length; i++) {
+            if (Character.isDigit(userCh[i])) {
+                char userCh1 = userCh[i];
+            }
+        }
+
+        int[] userDigit = new int[userCh.length];
+        for (int i = 0; i < userCh.length; i++) {
+            userDigit[i] = Character.getNumericValue(userCh[i]);
+        }
+
+        System.out.println("You entered following number:  ");
+        for (int i = 0; i < userDigit.length; i++) {
+            System.out.print(userDigit[i] + " ");
+        }
+        System.out.println();
+
+        int max = userDigit[0];
+        for (int i = 0; i < userDigit.length; i++) {
+            if (userDigit[i] > max) max = userDigit[i];
+        }
+
+        System.out.println("You digits in stars: ");
+        for (int i = 0; i < userDigit.length; i++) {
+            if (userDigit[i] == 0) {
+                createDigitZero();
+                System.out.println();
+            } else if (userDigit[i] == 1) {
+                createDigitOne();
+                System.out.println();
+            } else if (userDigit[i] == 2) {
+                createDigitTwo();
+                System.out.println();
+            } else if (userDigit[i] == 3) {
+                createDigitThree();
+                System.out.println();
+            } else if (userDigit[i] == 4) {
+                createDigitFour();
+                System.out.println();
+            } else if (userDigit[i] == 5) {
+                createDigitFive();
+                System.out.println();
+            } else if (userDigit[i] == 6) {
+                createDigitSix();
+                System.out.println();
+            } else if (userDigit[i] == 7) {
+                createDigitSeven();
+                System.out.println();
+            } else if (userDigit[i] == 8) {
+                createDigitEight();
+                System.out.println();
+            } else if (userDigit[i] == 9) {
+                createDigitNine();
+                System.out.println();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        numToStar();
+
+    }
+}
+
